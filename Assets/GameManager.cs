5b0e91da9bb5020c25c3f6f88f37e00fb09d5176
nadnbuds <lalam003@ucr.edu>
2017-05-22 00:20:31 -0700

    Some Comments, and GameManager Functionality

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,27 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-
+public class GameManager : Singleton<GameManager> {
+    bool pickEnabled;
     List<Card> currentPool;
+    Card targetCard;
     public int rounds;
     //Bounds for how many cards per round
     public int lowerBound, upperBound;
+    public int numOfChoices;
 
     private void Start()
     {
+        //How many rounds to go through in the game
         for (int x = 0; x < rounds; x++)
         {
             StartRound();
         }
     }
 
+    //The Body of each indivdual round
     private void StartRound()
     {
+        //Pull IFT cards into the current pool
         int numOfCards = Random.Range(lowerBound, upperBound);
         for(int x = 0; x < numOfCards; x++)
         {
             currentPool.Add(ObjectPooler.Instance.GetIFTObject());
         }
+        DisplayCards();
+        DisplayObjective(numOfCards);
+        for(int x = 0; x < numOfChoices - numOfCards; x++)
+        {
+            currentPool.Add(ObjectPooler.Instance.GetAllObject());
+        }
+        DisplayCards();
+    }
+
+    private void DisplayCards()
+    {
+
+    }
+
+    private void DisplayObjective(int num)
+    {
+        int target = Random.Range(1, num);
+        target -= 1;
+        targetCard = currentPool[target];
     }
 }
